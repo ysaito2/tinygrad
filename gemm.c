68fe3d4b8b57edeeb1fa43f40f7e2ae7dca8ebc5
ysaito2 <yutastevensaito@gmail.com>
2022-07-07 07:35:22 +0900

    vid 1:03:55

--- gemm.c
+++ gemm.c
@@ -2,12 +2,18 @@
 #include <time.h>
 #include <stdio.h>
 #include <assert.h>
+#include <immintrin.h>
 
 #define N 64 
 
 float A[N][N];
 float B[N][N];
 float C[N][N];
+float val[N][N];
+
+__m256 *Am = (__m256*)A;
+__m256 *Bm = (__m256*)B; 
+__m256 *Cm = (__m256*)C;
 
 uint64_t nanos(){
 	struct timespec start;
@@ -16,33 +22,62 @@ uint64_t nanos(){
 }
 
 
-#define BLOCK 32
+#define BLOCK 8 
 int main (){
 	assert(N%BLOCK == 0);	
-	
+
+	FILE *f = fopen("/tmp/matmul", "rb");
+	fread(A, 1, sizeof(float)*N*N, f);
+	fread(B, 1, sizeof(float)*N*N, f);
+	fread(val, 1, sizeof(float)*N*N, f);
+	fclose(f); 
+
+	// test
+	// for (int k=0; k<N*N; k++){
+	// 	A[k] = rand();
+	// 	B[k] = rand(); 
+	// }
+
 	uint64_t start = nanos();
 	for (int by = 0; by < N; by += BLOCK){
 		for (int bx = 0; bx < N; bx += BLOCK){
+		
 			float tc[BLOCK][BLOCK];
-			
-			// // compute
-			// for (int y = by; y < by+BLOCK; y++)	{
-			// 		for (int x = bx; x < bx+BLOCK; x++){
-			// 			float acc = 0;
-			// 			for (int k =0; k<N; k++){
-			// 				acc += A[y][k] * B[x][k];
-			// 			}
+			// compute
+			for (int y = by; y < BLOCK; y++)	{
+					for (int x = bx; x < BLOCK; x++){
+						float acc = 0;
+						for (int k =0; k<N; k++){
+							acc += A[by+y][k] * B[bx+x][k];
+						}
 
-			// 			tc[y][x] = acc;
-			// 		}
-			// }
+						tc[y][x] = acc;
+					}
+			}
 			
 			// store
-			for (int y = 0; y < BLOCK; y++) {
+			for (int y = 0; y < BLOCK; y++)	{
 				for (int x = 0; x < BLOCK; x++){
-					C[by+y][bx+x] = tc[y][x];
+					C[by+y][bx+x] = tc[y][x]; 
 				}
 			}
+			// __m256 tc[BLOCK]; 
+
+			// for (int y = 0; y < BLOCK; y++) {
+			// 	__m256 tmp;
+
+			// 	for (int k = 0; k < BLOCK; k+=8){
+			// 		tmp = _mm256_fmadd_ps(
+			// 			Am[((by+y)*N)/8 + k], 
+			// 			Bm[(bx*N)/8 + k], tmp);
+			// 	}
+
+			// 	tc[y] = tmp; 
+			// }
+
+			// for (int y=0; y < BLOCK; y++){
+			// 	Cm[((bx+y) *N)/8] = tc[y];
+			// }
 		}
 	}
 
@@ -52,6 +87,16 @@ int main (){
 	double s     = (end - start) * 1e-9;
 	
 	printf("%f GFLOP/S\n", gflop/s);
+
+	for (int y = 0; y < N; y++){	
+		for (int x = 0; x < N; x++)	{
+			if (C[y][x] != val[y][x]){
+				printf("Mismatch at %d x %d, %f != %f\n", y, x, C[y][x], val[y][x]);
+				return(-1); 
+			}
+		}
+	}
+
 	return 0;
 }
 
